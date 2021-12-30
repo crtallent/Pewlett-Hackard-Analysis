7fd02544b80280546aab5bba81eed64757756c8d
ctal <c_tallent@sbcglobal.net>
2021-12-30 10:47:08 -0600

    uploaded corrected csvs.

--- Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Query/Employee_Database_Challenge.sq1.sql
+++ Analysis Project Folder/Pewlett-Hackard-Analysis Folder/Queries/Query/Employee_Database_Challenge.sq1.sql
@@ -1,14 +1,12 @@
 --Retrieve emp_no, f_n, and l_n columns from emp table.
-SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+DROP TABLE retirement_titles;
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
 FROM employees AS e
 JOIN titles AS t
 ON (t.emp_no=e.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER by e.emp_no, t.title DESC;
---SELECT 41380
---Query returned successfully in 199 msec.
-SELECT * FROM retirement_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
@@ -61,3 +59,44 @@ WHERE (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;	
 
 SELECT * FROM mentorship_eligibility;
+
+---New Code Below----
+
+DROP TABLE retirement_titles;
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees AS e
+JOIN titles AS t
+ON (t.emp_no=e.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER by e.emp_no, t.title DESC;
+
+SELECT * FROM retirement_titles;
+DROP TABLE unique_titles;
+
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+retirement_titles.first_name,
+retirement_titles.last_name,
+retirement_titles.title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY retirement_titles.emp_no, retirement_titles.to_date DESC;
+
+SELECT * FROM unique_titles;
+DROP TABLE retiring_titles;
+
+--Retrieve # of titles from unique_titles
+SELECT COUNT (ut.title), ut.title
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY ut.count DESC;
+
+--Create retiring employees table - retiring titles
+SELECT ut.count, ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title 
+ORDER BY ut.count DESC;
+
+SELECT * FROM retiring_titles;
